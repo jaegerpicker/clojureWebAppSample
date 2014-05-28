be3ec1f724fd2e485e4289fb85f648e498839ad9
Shawn Campbell <jaegerpicker@gmail.com>
2014-05-28 15:46:42 -0400

    clojure work

--- shouter/project.clj
+++ shouter/project.clj
@@ -1,3 +1,9 @@
 (defproject shouter "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :description "Test app"
+  :url "http://github.com/jaegerpicker/clojureWebAppSample"
+  :min-lein-version "2.0.0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                  [org.clojure/java.jdbc "0.3.2"]
+                  [postgresql "9.1-901.jdbc4"]])
